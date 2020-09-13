0f3f8e420b9eb153b5a8a1d85b62a7fe791bda4c
Berlin Smith <berlin.smithjr@gmail.com>
2020-09-13 06:51:36 -0400

    working on server

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,23 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css'
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [password2, setPassword2] = useState();
+
+    const formHandler = ( event ) => {
+        let {name, value} = event.target;
+
+        switch (name) {
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            case 'password2':
+                setPassword2(value);
+                break;
+            default:
+                //
+        }
+    }
+
+    const register = (event) => {
+        event.preventDefault();
+        console.log('registering...');
+    }
+
+
     return (
         <div className="register">
-            <form className="register__form" method="POST" action="#">
+            <form className="register__form" onSubmit={register}>
                 <h1 className="register__formTitle">register</h1>
                 <p className="register__formError">Please enter your Email and/or Password and retry.</p>
                 <label htmlFor="email">
-                    <input type="email" name="email" id="email" className="register__formEmail" placeholder="Jane@Doe.com" aria-placeholder="email" />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        className="register__formEmail" 
+                        placeholder="Jane@Doe.com" 
+                        aria-placeholder="email"
+                        onChange={formHandler}
+                    />
                 </label>
                 <label htmlFor="password">
-                    <input type="password" name="password" id="password" className="register__formPassword" placeholder="Password" aria-placeholder="password" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        className="register__formPassword" 
+                        placeholder="Password"
+                        aria-placeholder="password" 
+                        onChange={formHandler}
+                    />
                 </label>
                 <label htmlFor="password2">
-                    <input type="password" name="password2" id="password2" className="register__formPassword" placeholder="Password2" aria-placeholder="password2" />
+                    <input 
+                        type="password" 
+                        name="password2" 
+                        id="password2" 
+                        className="register__formPassword" 
+                        placeholder="Password2" 
+                        aria-placeholder="password2"
+                        onChange={formHandler}
+                    />
                 </label>
-                <button type="button" className="register__formButton">register</button>
+                <button type="submit" className="register__formButton">register</button>
                 <p className="register__formLink"><Link to="/login">Login</Link></p>
             </form>
             
--- server/server.py
+++ server/server.py
@@ -1,6 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'FE2F7488321C5D9C7063441D7882E8ADE772179B36C2F'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secret123@localhost/trading'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    created = db.Column(db.DateTime)
+    updated = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return F"User('{self.email}')"
+
+@app.route('/login', methods=['POST'])
+def login():
+    print(request)
+    return 'hello'
