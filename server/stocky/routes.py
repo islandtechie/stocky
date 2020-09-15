1c34727091cd124705da94a2e8bb7e8413b7e106
Berlin Smith <berlin.smithjr@gmail.com>
2020-09-14 21:45:18 -0400

    working on server

--- server/stocky/routes.py
+++ server/stocky/routes.py
@@ -1,9 +1,9 @@
 import datetime
-from flask import request
+from flask import request, flash
+from sqlalchemy import exc
 from werkzeug.security import generate_password_hash, check_password_hash
 from stocky import app, db, ma
-from marshmallow import ValidationError
-from stocky.models import User, UserSchema
+from stocky.models import User
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -13,20 +13,23 @@ def login():
 @app.route('/register', methods=['POST'])
 def register():
     data = request.form
-    if data['password'] == data['password2']:
-        user = User(
-            email=data['username'],
-            password=data['password'],
-            created=datetime.datetime.now(),
-            updated=datetime.datetime.now()
-        )
 
-        try:
-        
-            db.session.add(user)
-            db.session.commit()
+    user = User(
+        email=data['email'],
+        password= generate_password_hash(data['password'], method='pbkdf2:sha256', salt_length=12),
+        created=datetime.datetime.now(),
+        updated=datetime.datetime.now()
+    )
 
-            return 'added'
-        except:
-            db.session.rollback()
-    return data
+    try:
+        db.session.add(user)
+        db.session.commit()
+
+        return 'User created sucessfully', 201
+
+    except exc.DBAPIError as e:
+        print(e.)
+        return 'Please try again. SOmething went wrong', 500
+
+        pass
+
